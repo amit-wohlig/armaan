03a911c7a8dff8d0e3bc70669a2d26332bc5c839
JasmiRathod <jasmirathod93@gmail.com>
2015-08-07 17:37:35 +0530

    all done hopefully -_-

--- js/controllers.js
+++ js/controllers.js
@@ -1,6 +1,6 @@
-angular.module('phonecatControllers', ['templateservicemod', 'navigationservice', 'ui.bootstrap', 'cfp.loadingBar', 'infinite-scroll', 'toaster', 'ngAnimate', 'ngAutocomplete', 'ngTagsInput', 'ngDialog', 'ngSocial', 'valdr', 'ngSanitize', 'ui.select','angular-flexslider'])
+angular.module('phonecatControllers', ['templateservicemod', 'navigationservice', 'ui.bootstrap', 'cfp.loadingBar', 'infinite-scroll', 'toaster', 'ngAnimate', 'ngAutocomplete', 'ngTagsInput', 'ngDialog', 'ngSocial', 'valdr', 'ngSanitize', 'ui.select', 'angular-flexslider'])
 
-.controller('AboutusCtrl', function($scope, TemplateService, NavigationService, cfpLoadingBar, $timeout) {
+.controller('AboutusCtrl', function ($scope, TemplateService, NavigationService, cfpLoadingBar, $timeout) {
     //Used to name the .html file
     $scope.template = TemplateService.changecontent("aboutus");
     $scope.menutitle = NavigationService.makeactive("ABOUT US");
@@ -8,47 +8,50 @@ angular.module('phonecatControllers', ['templateservicemod', 'navigationservice'
     $scope.navigation = NavigationService.getnav();
 })
 
-.controller('AvailableatCtrl', function($scope, TemplateService, NavigationService) {
+.controller('AvailableatCtrl', function ($scope, TemplateService, NavigationService) {
     $scope.template = TemplateService.changecontent("availableat");
     $scope.menutitle = NavigationService.makeactive("AVAILABLE AT");
     TemplateService.title = $scope.menutitle;
     $scope.navigation = NavigationService.getnav();
 })
 
-.controller('ContactCtrl', function($scope, TemplateService, NavigationService) {
+.controller('ContactCtrl', function ($scope, TemplateService, NavigationService) {
     $scope.template = TemplateService.changecontent("contact");
     $scope.menutitle = NavigationService.makeactive("CONTACT");
     TemplateService.title = $scope.menutitle;
     $scope.navigation = NavigationService.getnav();
 })
 
-.controller('MediaCtrl', function($scope, TemplateService, NavigationService) {
+.controller('MediaCtrl', function ($scope, TemplateService, NavigationService) {
     $scope.template = TemplateService.changecontent("media");
     $scope.menutitle = NavigationService.makeactive("MEDIA");
     TemplateService.title = $scope.menutitle;
     $scope.navigation = NavigationService.getnav();
 })
 
-.controller('ProductCtrl', function($scope, TemplateService, NavigationService) {
+.controller('ProductCtrl', function ($scope, TemplateService, NavigationService) {
     $scope.template = TemplateService.changecontent("product");
     $scope.menutitle = NavigationService.makeactive("PRODUCTS");
     TemplateService.title = $scope.menutitle;
     $scope.navigation = NavigationService.getnav();
+    
+    $scope.hidenav = 'hidenav';
 })
-    .controller('FeedbackCtrl', function($scope, TemplateService, NavigationService) {
+
+.controller('FeedbackCtrl', function ($scope, TemplateService, NavigationService) {
     $scope.template = TemplateService.changecontent("feedback");
     $scope.menutitle = NavigationService.makeactive("FEEDBACK");
     TemplateService.title = $scope.menutitle;
     $scope.navigation = NavigationService.getnav();
 })
 
-.controller('headerctrl', function($scope, TemplateService) {
+.controller('headerctrl', function ($scope, TemplateService) {
     $scope.template = TemplateService;
 })
 
 .controller('sliderctrl', function ($scope, TemplateService) {
     $scope.template = TemplateService;
-    
+
     $scope.slides = [
     'img/slider/1.jpg',
     'img/slider/2.jpg',
