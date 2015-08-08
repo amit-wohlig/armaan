c16aaf6013d12006a0906d68206b95cc092b4d53
Tushar <tushar@wohlig.com>
2015-08-08 19:44:41 +0530

    Beta Live

--- js/app.js
+++ js/app.js
@@ -66,14 +66,20 @@ firstapp.directive('img', function($compile, $parse) {
         restrict: 'E',
         replace: false,
         link: function($scope, element, attrs) {
-            $element = $(element);
-            $element.after("<img src='img/loading.gif' class='loading' />");
-            var $loading = $element.next(".loading");
-            $element.load(function() {
-                $loading.remove();
-                $(this).addClass("doneLoading");
-                console.log($element.css("opacity"));
-            });
+            var $element = $(element);
+            if(!attrs.noloading)
+            {
+                $element.after("<img src='img/loading.gif' class='loading' />");
+                var $loading = $element.next(".loading");
+                $element.load(function() {
+                    $loading.remove();
+                    $(this).addClass("doneLoading");
+                });
+            }
+            else
+            {
+                $($element).addClass("doneLoading");
+            }
         }
     };
 });
